1cc1a7de7ce8990f37d8eabb32b68db2b7694a01
Craig <cban0901@gmail.com>
2025-10-28 14:58:31 +0000

    Refetching data with tags to update stale store albums data after POST request made using RTK Query

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -6,7 +6,6 @@ import Button from "./Button";
 export default function AlbumsList({user}) {
   const {data, error, isLoading} = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
-  console.log(results);
 
   const handleAddAlbum = () => {
     addAlbum(user)
