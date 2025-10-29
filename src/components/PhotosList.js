309fdb8b3d75da4c128aeee2b442da619a868b7c
Craig <cban0901@gmail.com>
2025-10-29 15:19:52 +0000

    adding photo

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,10 +1,16 @@
 import PhotosListItem from "./PhotosListItem";
-import {useFetchPhotosQuery} from "../store";
+import {useFetchPhotosQuery, useAddPhotoMutation} from "../store";
 import Skeleton from "./Skeleton";
+import Button from "./Button";
 
 export default function PhotosList({album}) {
   const {data, error, isFetching} = useFetchPhotosQuery(album);
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
   console.log(data)
+
+  const handleAddPhoto = () => {
+    addPhoto(album)
+  }
 
   let content;
   if (isFetching) {
@@ -19,7 +25,11 @@ export default function PhotosList({album}) {
 
   return (
     <div>
-      {content}
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Photos in {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>Add Photo</Button>
+        {content}
+      </div>
     </div>
   )
 }
