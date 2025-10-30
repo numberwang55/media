784bb43846b2edc7375379b66bf799ff5399412f
Craig <cban0901@gmail.com>
2025-10-30 10:31:47 +0000

    automated data refetching

--- src/store/apis/photosApi.js
+++ src/store/apis/photosApi.js
@@ -18,6 +18,15 @@ const photosApi = createApi({
   endpoints(builder) {
     return {
       fetchPhotos: builder.query({
+        providesTags: (result, error, album) => {
+          const tags = result.map((photo) => {
+            return { type: "Photo", id: photo.id };
+          })
+          tags.push(
+            {type: "AlbumsPhotos", albumId: album.id }
+          )
+          return tags;
+        },
         query: (album) => {
           return {
             url: "/photos",
@@ -29,6 +38,9 @@ const photosApi = createApi({
         }
       }),
       addPhoto: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "AlbumsPhotos", albumId: album.id }]
+        },
         query: (album) => {
           return {
             url: "/photos",
@@ -41,6 +53,9 @@ const photosApi = createApi({
         }
       }),
       deletePhoto: builder.mutation({
+        invalidatesTags: (result, error, photo) => {
+          return [{ type: "Photo", id: photo.id }]
+        },
         query: (photo) => {
           return {
             url: `/photos/${photo.id}`,
